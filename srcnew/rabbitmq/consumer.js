8c443ea3d0e3639af94e18bdfc7cf73e448ebee5
TrinhTuan-2002-fpt <tuantu2k2badaoyl@gmail.com>
2024-10-02 08:05:45 +0700

    doc redis , rabbitmq demo

---
+++ srcnew/rabbitmq/consumer.js
@@ -0,0 +1,28 @@
+const amqp = require('amqplib');
+
+const messages = 'hello, RabbitMQ for Tuantm';
+
+const runProducer = async () => {
+  try {
+    const connection = await amqp.connect('amqp://guest:12345@localhost');
+    const channel = await connection.createChannel();
+
+    const queueName = 'test-topic';
+    await channel.assertQueue(queueName, { durable: true });
+
+    channel.consume(
+      queueName,
+      (messages) => {
+        console.log(`Received ${messages.content.toString()}`);
+      },
+      {
+        noAck: true,
+      }
+    );
+    console.log(`messages send: ${messages}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runProducer().catch(console.error);
--- srcnew/rabbitmq/producer.js
+++ srcnew/rabbitmq/producer.js
@@ -1,1 +1,23 @@
-const amqp = require('amqplib')
+const amqp = require('amqplib');
+
+const messages = 'hello, RabbitMQ for Tuantm';
+
+const runProducer = async () => {
+  try {
+    const connection = await amqp.connect('amqp://guest:12345@localhost');
+    const channel = await connection.createChannel();
+
+    const queueName = 'test-topic';
+    await channel.assertQueue(queueName, { durable: true });
+
+    channel.sendToQueue(queueName, Buffer.from(messages));
+    console.log(`messages send: ${messages}`);
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runProducer().catch(console.error);
